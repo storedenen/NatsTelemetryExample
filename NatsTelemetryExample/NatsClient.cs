0e05fe25e4c21e0f7b67178221f3099bbd9df5e9
goblinus <storedenen@gmail.com>
2024-06-09 11:53:57 +0200

    added nats client

---
+++ NatsTelemetryExample/NatsClient.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace NatsTelemetryExample
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using NATS.Client.Core;
+    using NATS.Client.Serializers.Json;
+
+    public class NatsClient : BackgroundService
+    {
+        private readonly ILogger<NatsClient> _logger;
+        private readonly NatsOpts _natsOptions;
+        private readonly string? _publisherSubject;
+        private NatsConnection _natsConnection;
+        private INatsSub<object> _publisherSubscription;
+
+        public NatsClient(ILogger<NatsClient> logger, IConfiguration configuration)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _publisherSubject = configuration.GetValue("NATS_PUBLISHER_SUBJECT", "publisher");
+            var url = configuration.GetValue("NATS_URL", "127.0.0.1:4222");
+            var clientName = configuration.GetValue("NATS_CLIENT_NAME", "NATS-Client1");
+            var password = configuration.GetValue("NATS_PASSWORD", "pass");
+            var username = configuration.GetValue("NATS_PASSWORD", "user");
+
+            _natsOptions = new NatsOpts
+            {
+                Url = url,
+                LoggerFactory = LoggerFactory.Create(x => x.AddConsole()),
+                SerializerRegistry = NatsJsonSerializerRegistry.Default,
+                Name = clientName,
+                AuthOpts = new NatsAuthOpts()
+                {
+                    Password = password,
+                    Username = username,
+                }
+            };
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await foreach (var msg in _publisherSubscription.Msgs.ReadAllAsync(stoppingToken))
+                    {
+                        var order = msg.Data;
+                        _logger.LogInformation("Subscriber received {Subject}: {Order}", msg.Subject, order);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    _logger.LogError(exception, "An error occured");
+                }
+            }
+            
+            _logger.LogInformation("Stopping...");
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _natsConnection = new NatsConnection(_natsOptions);
+            _publisherSubscription = await _natsConnection.SubscribeCoreAsync<Object>(_publisherSubject);
+            await base.StartAsync(cancellationToken);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await base.StopAsync(cancellationToken);
+            await _publisherSubscription.DisposeAsync();
+            await _natsConnection.DisposeAsync();
+        }
+    }
+}
--- NatsTelemetryExample/Program.cs
+++ NatsTelemetryExample/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace NatsTelemetryExample;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-class Program
+namespace NatsTelemetryExample
 {
-    static void Main(string[] args)
+    using System;
+    using Microsoft.Extensions.Hosting;
+    using NATS.Client.Hosting;
+    
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+            
+            var hostBuilder = new HostBuilder();
+            
+            hostBuilder.ConfigureServices(x =>
+            {
+                x.AddLogging(x =>
+                {
+                    x.AddConsole();
+                });
+                x.AddNats();
+                x.AddHostedService<NatsClient>();
+            });
+
+            var host = hostBuilder.Build();
+        
+            host.Run();
+        }
     }
 }
